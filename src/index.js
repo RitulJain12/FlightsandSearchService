46df62a7325301b04cb4916da7887d9ac7b0bbd8
Ritul Jain <rituljain202@gmail.com>
2026-01-02 11:24:48 +0530

    complete setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express=require('express');
-const {PORT}=require('./config/server-config')
+const {PORT}=require('./config/server-config');
+const bodyParser = require('body-parser');
 const setupAndStartServer=async()=>{
     const app=express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
         console.log(`Server Started at ${PORT}`);
     })

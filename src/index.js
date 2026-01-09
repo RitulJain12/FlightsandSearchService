47a0a0967e928257c2be23eff04c38a086176c51
Ritul Jain <rituljain202@gmail.com>
2026-01-09 12:41:07 +0530

    models and migrations

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const express=require('express');
 const {PORT}=require('./config/server-config');
 const bodyParser = require('body-parser');
+
+
+
 const setupAndStartServer=async()=>{
-    const app=express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
-    app.listen(PORT,()=>{
+const app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.listen(PORT,()=>{
         console.log(`Server Started at ${PORT}`);
     })
+
+
+
 }
 
 
